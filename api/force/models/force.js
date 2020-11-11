71f58f6d08edf9e5114d40104de2df70c75639af
madhulikagadang <gadangmadhulika@gmail.com>
2020-11-11 16:17:18 +0530

    check

--- api/force/models/force.js
+++ api/force/models/force.js
@@ -5,4 +5,8 @@
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+   force: {id:1,title:"sfe",singlephase:"rfe",
+    threephase:"ertg",published_at:"2020-11-10T13:04:35.186Z",created_at:"2020-11-10T09:50:03.512Z",
+    updated_at:"2020-11-10T13:04:35.201Z"}
+};
