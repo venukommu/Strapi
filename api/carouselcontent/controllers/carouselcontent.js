35a913b72b2fb2ac9a9b35cb49e6a8f98bbb3f42
madhulikagadang <gadangmadhulika@gmail.com>
2020-11-16 17:24:31 +0530

    single type content controller

--- api/carouselcontent/controllers/carouselcontent.js
+++ api/carouselcontent/controllers/carouselcontent.js
@@ -1,8 +1,18 @@
-'use strict';
-
+const { sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async find(ctx) {
+        let entities;
+        if (ctx.query._q) {
+          entities = await strapi.services.carouselcontent.search(ctx.query);
+        } else {
+          entities = await strapi.services.carouselcontent.find(ctx.query);
+        }
+    
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.carouselcontent }));
+      },
+};
--- api/homebanner/controllers/homebanner.js
+++ api/homebanner/controllers/homebanner.js
@@ -1,8 +1,25 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
+
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.homebanner.search(ctx.query);
+    } else {
+      entities = await strapi.services.homebanner.find(ctx.query);
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.homebanner }));
+  },
+};
