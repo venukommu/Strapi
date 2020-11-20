09f89142c4614e8b1691f8f5c456ae47a7b8c6f5
madhulikagadang <gadangmadhulika@gmail.com>
2020-11-20 11:57:22 +0530

    image issue

--- api/awesomefeatures/controllers/awesomefeatures.js
+++ api/awesomefeatures/controllers/awesomefeatures.js
@@ -1,25 +1,21 @@
-const { sanitizeEntity } = require('strapi-utils');
+'use strict';
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {
-    /**
-   * Retrieve records.
-   *
-   * @return {Array}
-   */
+    module.exports = {
+        async find(ctx) {
+            let entities;
+            if (ctx.query._q) {
+              entities = await strapi.services.awesomefeatures.search(ctx.query);
+            } else {
+              entities = await strapi.services.awesomefeatures.find(ctx.query);
+            }
+            return sanitizeEntity(entities, { model: strapi.models.awesomefeatures });
+            //return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.carouselcontent }));
+          },
+    };
 
-  async find(ctx) {
-    let entities;
-    if (ctx.query._q) {
-      entities = await strapi.services.awesomefeatures.search(ctx.query);
-    } else {
-      entities = await strapi.services.awesomefeatures.find(ctx.query);
-    }
-    return sanitizeEntity(entities, { model: strapi.models.awesomefeatures });
-    //return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.homebanner }));
-  },
-};
+    
