d9218709df6a2d9652ef1aa347429a67a40bf405
KasaSandhya <svanapalli@baylogictech.com>
2020-12-11 16:35:52 +0530

    changed enposs product model name

--- api/force/models/force.js
+++ api/force/models/force.js
@@ -5,8 +5,4 @@
  * to customize this model
  */
 
-module.exports = {
-   force: {id:1,title:"sfe",singlephase:"rfe",
-    threephase:"ertg",published_at:"2020-11-10T13:04:35.186Z",created_at:"2020-11-10T09:50:03.512Z",
-    updated_at:"2020-11-10T13:04:35.201Z"}
-};
+module.exports = {};
