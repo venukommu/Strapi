87b1f88e14eae4816d1dece84706b26f7c100d8a
KasaSandhya <svanapalli@baylogictech.com>
2021-01-22 16:42:44 +0530

    moved stripe customer variable to try block

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -19,57 +19,57 @@ module.exports = {
     //console.log(ctx.request.body);
     const { product, token } =  ctx.request.body;
 
-    const customer = await stripe.customers.create({
-      email: token.email,
-      source: token.id 
-    })
-   
     // charge on stripe
     try {
-    const charge = await stripe.charges.create({
+      const customer = await stripe.customers.create({
+        email: token.email,
+        source: token.id 
+      })
+      const charge = await stripe.charges.create({
       // Transform cents to dollars.
-      amount: product.price,
-      currency: "usd",
-      customer: customer.id,
-      receipt_email: token.email,
-      description: "All Products Description",
-      shipping: {
-        name: token.card.name,
-        address: {
-          line1: token.card.address_line1,
-          line2: token.card.address_line2,
-          city: token.card.address_city,
-          country: token.card.country,
-          postal_code: token.card.address_zip
+        amount: product.price,
+        currency: "usd",
+        customer: customer.id,
+        receipt_email: token.email,
+        description: "All Products Description",
+        shipping: {
+          name: token.card.name,
+          address: {
+            line1: token.card.address_line1,
+            line2: token.card.address_line2,
+            city: token.card.address_city,
+            country: token.card.country,
+            postal_code: token.card.address_zip
+          }
         }
-      }
     });
     //console.log(charge);
     // Register the order in the database
     try {
-    const order = await strapi.services.order.create({
-      //user: ctx.state.user.id,
-      charge_id: charge.id,
-      name: token.card.name,
-      amount: charge.amount,
-      city: token.card.address_city,
-      state: token.card.address_state,
-      token: token.id,
-      status: charge.status,
-      billingaddress: charge.billing_details.address,
-      shippingaddress: charge.shipping.address
-    });
-    return order;
-  } catch (err) {
-    console.log('order error: ', err);
-  }
+      const order = await strapi.services.order.create({
+        //user: ctx.state.user.id,
+        charge_id: charge.id,
+        name: token.card.name,
+        amount: charge.amount,
+        city: token.card.address_city,
+        state: token.card.address_state,
+        token: token.id,
+        status: charge.status,
+        billingaddress: charge.billing_details.address,
+        shippingaddress: charge.shipping.address
+      });
+      //console.log("responce",ctx);
+      return order;
+    } catch (err) {
+      console.log('order error: ', err);
+    }
   } catch (err) {
     console.log('stripe error: ', err);
     ctx.response.status = 400;
     ctx.response.message = err.raw.message;
     ctx.response.body = err;
     return ctx.response;
-  }
+    }
   },
 };
 
